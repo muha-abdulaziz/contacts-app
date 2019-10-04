8a45000aeb0c59c79c3076f93462ea843b279d23
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-04 10:50:04 +0200

    Document the responseBody module

--- api/utils/responseBody.js
+++ api/utils/responseBody.js
@@ -1,3 +1,16 @@
+/**
+ * This module to simplify the change of response body
+ */
+
+/**
+ * Generate response body
+ *
+ * @param {string} message - short message describs the status of the request
+ * @param {*} data - any thing need to send to the client
+ * @param {*} [error] - error message and/or stack [NOT-for-PRODUCTION]
+ *
+ * @returns {Object}
+ */
 module.exports = (message, data, error) => {
   if (!message) throw new Error('message is required');
   return {
