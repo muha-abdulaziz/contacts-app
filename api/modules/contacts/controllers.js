de9fd9a361576b62c3e622524973d2e412c791e3
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-05 00:35:23 +0200

    Implement GET method for /contacts

--- api/modules/contacts/controllers.js
+++ api/modules/contacts/controllers.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const responseBody = require('../../utils/responseBody');
+const contactModel = require('./contacts.model');
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  return res.status(501).send('Not Implemented');
+router.get('/', async (_req, res) => {
+  try {
+    const contacts = await contactModel.find();
+
+    const resMesg = 'contacts retrived successfuly';
+    const resBody = responseBody(resMesg, contacts);
+
+    return res.json(resBody);
+  } catch (err) {
+    const resBody = responseBody('Server Error');
+
+    return res.status(500).json(resBody);
+  }
 });
 
 module.exports = router;
