5d5de6e0ecc79db68f0324dcd3a7c686aa9011e1
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-04 22:21:21 +0200

    Init test for contact module

---
+++ test/unit/contactsModule.test.js
@@ -0,0 +1,18 @@
+// set environement to test, to use test configuration
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('Test contacts model functionality', function() {
+  it('has add method');
+
+  it('has find method');
+
+  it('has findById method');
+
+  it('has update method');
+
+  it('has delete method');
+});
