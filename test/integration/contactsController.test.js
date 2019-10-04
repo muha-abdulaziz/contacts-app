a522efd4eaa2e8d8dccfe01e28c461ea15037ed0
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-05 00:21:52 +0200

    Add more tests to contacts integration test.   - GET method respond  with 200   - GET method respond with expected data

--- test/integration/contactsController.test.js
+++ test/integration/contactsController.test.js
@@ -39,4 +39,36 @@ describe('Check Authintication of /contacts', function checkAuth() {
       });
     }); // end of for each methods
   }); // end of notAuthorize function
+
+  describe('allow access when the token is valid', function() {
+    it('GET /contacts', async function() {
+      await chai
+        .request(app)
+        .get('/contacts')
+        .set('Authorisation', 'MY_SECRET_ENCRYPTED_TOKEN')
+        .then(res => {
+          expect(res).to.have.status(200);
+        });
+    });
+  });
 }); // end of checkAuth function
+
+describe('test /contacts functionality', function() {
+  let token;
+  before(() => {
+    token = 'MY_SECRET_ENCRYPTED_TOKEN';
+  });
+
+  it('retreve contacts successfuly', async function() {
+    await chai
+      .request(app)
+      .get('/contacts')
+      .set('Authorisation', token)
+      .then(res => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.has.property('data');
+        expect(res.body.data).to.be.an('array');
+      });
+  });
+});
