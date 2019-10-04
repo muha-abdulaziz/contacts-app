9a32b49511e144b9f7518088ab52cf9d1a88882a
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-04 23:01:31 +0200

    Add test to check the interface of contact.model

--- test/unit/contactsModule.test.js
+++ test/unit/contactsModule.test.js
@@ -3,16 +3,33 @@ process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 
+const contactModel = require('../../api/modules/contacts/contacts.model');
+
 const { expect } = chai;
 
 describe('Test contacts model functionality', function() {
-  it('has add method');
+  it('has add method', function() {
+    expect(contactModel).to.have.property('add');
+    expect(contactModel.add).to.be.a('function');
+  });
 
-  it('has find method');
+  it('has find method', function() {
+    expect(contactModel).to.have.property('find');
+    expect(contactModel.add).to.be.a('function');
+  });
 
-  it('has findById method');
+  it('has findById method', function() {
+    expect(contactModel).to.have.property('findById');
+    expect(contactModel.findById).to.be.a('function');
+  });
 
-  it('has update method');
+  it('has update method', function() {
+    expect(contactModel).to.have.property('update');
+    expect(contactModel.update).to.be.a('function');
+  });
 
-  it('has delete method');
+  it('has delete method', function() {
+    expect(contactModel).to.have.property('delete');
+    expect(contactModel.delete).to.be.a('function');
+  });
 });
