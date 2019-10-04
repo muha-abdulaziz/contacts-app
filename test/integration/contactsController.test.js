4986b8b95815c079eb41d988862e8a2b124d07fa
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-04 07:40:43 +0200

    Init integration test for contacts controller Init test for not provide an Authorisation header Init test for invalid token

---
+++ test/integration/contactsController.test.js
@@ -0,0 +1,27 @@
+// set environement to test, to use test configuration
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+const app = require('../../app');
+
+describe('Check Authintication of /contacts', function checkAuth() {
+  describe('must forbidden when no Authorisation header', function forbidden() {
+    const methods = ['get', 'post'];
+    methods.forEach(method => {
+      it(`${method} /contacts`);
+    }); // end of for each methods
+  }); // end of forbidden function
+
+  describe('must not authorize when invalid token provided', function notAuthorize() {
+    const methods = ['get', 'post'];
+    methods.forEach(method => {
+      it(`${method} /contacts`);
+    }); // end of for each methods
+  }); // end of notAuthorize function
+}); // end of checkAuth function
