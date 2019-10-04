fca7bc24f71d14fdf904d7110e8bca94e733a6e0
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-04 21:37:26 +0200

    Check if the provided accessToken is valid in the isAuthorized If the accessToken is valid allow the clint to access the   protected resources

--- api/middelwares/isAuthorized.js
+++ api/middelwares/isAuthorized.js
@@ -1,4 +1,9 @@
+/**
+ * Gives a client access to resourse if s/he authorized
+ */
+
 const responseBody = require('../utils/responseBody');
+const accessToken = require('../utils/accessToken');
 
 module.exports = (req, res, next) => {
   if (!req.headers.authorisation) {
@@ -8,5 +13,23 @@ module.exports = (req, res, next) => {
     return res.status(403).send(resBody);
   }
 
-  next();
+  if (req.headers.authorisation) {
+    const token = 'req.headers.authorisation';
+    if (accessToken.verify(token)) {
+      // add user data (mock the token payload)
+      req.user = {
+        id: 1,
+      };
+
+      next();
+    }
+
+    const resMesg = 'You Can not use this resource';
+    const resBody = responseBody(resMesg);
+
+    // indicates the authentication scheme
+    res.set('WWW-Authenticate', 'test realm="contacts"');
+
+    return res.status(401).send(resBody);
+  }
 };
