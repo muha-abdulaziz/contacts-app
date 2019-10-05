dd488009c174d69adcb63d69790dd5950083bf59
muha-abdulaziz <muha.abdulaziz@gmail.com>
2019-10-05 02:05:19 +0200

    Implement add method of contacts model

--- api/modules/contacts/contacts.model.js
+++ api/modules/contacts/contacts.model.js
@@ -1,7 +1,19 @@
 const contacts = [];
 
+let lastId = 0;
+
 module.exports = {
-  add: contact => {},
+  add: contact => {
+    return new Promise((resolve, reject) => {
+      if (!contact) return reject(new TypeError('Missing param: "contact"'));
+
+      lastId += 1;
+      contact.id = lastId;
+      contacts.push(contact);
+
+      return resolve(contact);
+    });
+  },
 
   find: () => {
     return new Promise(resolve => {
